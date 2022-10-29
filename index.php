f2740e4892232432eade7839bc26c6254d1cb7ec
Mylka18 <1356729@senati.pe>
2022-10-29 17:21:37 -0500

    Cambio de index.php

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
-<?php
-$edad = $_REQUEST['age'];
-$NOMBRE = $_REQUEST['name'];
-
-echo "El nombre es: $nombre y la edad es $edad años";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHPH HTML</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+    <h2>PHP</h2>
+    <?php echo "Como estas?"; ?>
+</body>
+</html>
